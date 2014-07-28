921cf831ecb64740818a228dda8d4d048d8db8aa
Leo Esguerra <leo.esguerra@gmail.com>
2014-07-29 01:18:24 +0800

    + Images, scripts and main scene file

---
+++ Assets/scripts/MouseCameraControl.cs
@@ -0,0 +1,169 @@
+﻿//
+//Filename: MouseCameraControl.cs
+//
+
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Mouse")]
+public class MouseCameraControl : MonoBehaviour
+{ 
+	public float orthoZoomSensitivity = 5.0f;
+	public float orthoZoomSpeed = 3.0f;
+	public float orthoZoomMin = 1.5f;
+	public float orthoZoomMax= 8.0f;
+
+	public float perspectiveZoomSensitivity= 30.0f;
+	public float perspectiveZoomSpeed= 5.0f;
+	public float perspectiveZoomMin= 15.0f;
+	public float perspectiveZoomMax= 80.0f;
+
+	private float zoom;
+	private float zoomSensitivity;
+	private float zoomSpeed;
+	private float zoomMin;
+	private float zoomMax;
+
+	// Mouse buttons in the same order as Unity
+	public enum MouseButton { Left = 0, Right = 1, Middle = 2, None = 3 }
+	
+	[System.Serializable]
+	// Handles left modifiers keys (Alt, Ctrl, Shift)
+	public class Modifiers
+	{
+		public bool leftAlt;
+		public bool leftControl;
+		public bool leftShift;
+		
+		public bool checkModifiers()
+		{
+			return (!leftAlt ^ Input.GetKey(KeyCode.LeftAlt)) &&
+				(!leftControl ^ Input.GetKey(KeyCode.LeftControl)) &&
+					(!leftShift ^ Input.GetKey(KeyCode.LeftShift));
+		}
+	}
+	
+	[System.Serializable]
+	// Handles common parameters for translations and rotations
+	public class MouseControlConfiguration
+	{
+		public bool activate;
+		public MouseButton mouseButton;
+		public Modifiers modifiers;
+		public float sensitivity;
+		
+		public bool isActivated()
+		{
+			return activate && Input.GetMouseButton((int)mouseButton) && modifiers.checkModifiers();
+		}
+	}
+	
+	[System.Serializable]
+	// Handles scroll parameters
+	public class MouseScrollConfiguration
+	{
+		public bool activate;
+		public Modifiers modifiers;
+		public float sensitivity;
+		
+		public bool isActivated()
+		{
+			return activate && modifiers.checkModifiers();
+		}
+	}
+	
+	// Yaw default configuration
+	public MouseControlConfiguration yaw = new MouseControlConfiguration { mouseButton = MouseButton.Right, sensitivity = 10F };
+	
+	// Pitch default configuration
+	public MouseControlConfiguration pitch = new MouseControlConfiguration { mouseButton = MouseButton.Right, modifiers = new Modifiers{ leftControl = true }, sensitivity = 10F };
+	
+	// Roll default configuration
+	public MouseControlConfiguration roll = new MouseControlConfiguration();
+	
+	// Vertical translation default configuration
+	public MouseControlConfiguration verticalTranslation = new MouseControlConfiguration { mouseButton = MouseButton.Middle, sensitivity = 2F };
+	
+	// Horizontal translation default configuration
+	public MouseControlConfiguration horizontalTranslation = new MouseControlConfiguration { mouseButton = MouseButton.Middle, sensitivity = 2F };
+	
+	// Depth (forward/backward) translation default configuration
+	public MouseControlConfiguration depthTranslation = new MouseControlConfiguration { mouseButton = MouseButton.Left, sensitivity = 2F };
+	
+	// Scroll default configuration
+	public MouseScrollConfiguration scroll = new MouseScrollConfiguration { sensitivity = 2F };
+	
+	// Default unity names for mouse axes
+	public string mouseHorizontalAxisName = "Mouse X";
+	public string mouseVerticalAxisName = "Mouse Y";
+	public string scrollAxisName = "Mouse ScrollWheel";
+
+	void Start() 
+	{
+		if (camera.isOrthoGraphic)
+		{
+			zoom = camera.orthographicSize;
+			zoomSensitivity = orthoZoomSensitivity;
+			zoomSpeed = orthoZoomSpeed;
+			zoomMin = orthoZoomMin;
+			zoomMax = orthoZoomMax;
+		} else {
+			zoom = camera.fieldOfView;
+			zoomSensitivity = perspectiveZoomSensitivity;
+			zoomSpeed = perspectiveZoomSpeed;
+			zoomMin = perspectiveZoomMin;
+			zoomMax = perspectiveZoomMax;
+		}
+	}
+	
+	void Update()
+	{
+		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+		zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+	}
+
+	void LateUpdate ()
+	{
+		if (yaw.isActivated())
+		{
+			float rotationX = Input.GetAxis(mouseHorizontalAxisName) * yaw.sensitivity;
+			transform.Rotate(0, rotationX, 0);
+		}
+		if (pitch.isActivated())
+		{
+			float rotationY = Input.GetAxis(mouseVerticalAxisName) * pitch.sensitivity;
+			transform.Rotate(-rotationY, 0, 0);
+		}
+		if (roll.isActivated())
+		{
+			float rotationZ = Input.GetAxis(mouseHorizontalAxisName) * roll.sensitivity;
+			transform.Rotate(0, 0, rotationZ);
+		}
+		
+		if (verticalTranslation.isActivated())
+		{
+			float translateY = Input.GetAxis(mouseVerticalAxisName) * verticalTranslation.sensitivity;
+			transform.Translate(0, translateY, 0);
+		}
+		
+		if (horizontalTranslation.isActivated())
+		{
+			float translateX = Input.GetAxis(mouseHorizontalAxisName) * horizontalTranslation.sensitivity;
+			transform.Translate(translateX, 0, 0);
+		}
+		
+		if (depthTranslation.isActivated())
+		{
+			float translateZ = Input.GetAxis(mouseVerticalAxisName) * depthTranslation.sensitivity;
+			transform.Translate(0, 0, translateZ);
+		}
+		
+		if (scroll.isActivated())
+		{
+			if (camera.isOrthoGraphic)
+				camera.orthographicSize = Mathf.Lerp (camera.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
+			else
+				camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoom, Time.deltaTime * zoomSpeed);
+		}
+	}
+	
+}
---
+++ Assets/scripts/TouchCameraControl.cs
@@ -0,0 +1,63 @@
+﻿//
+//Filename: TouchCameraControl.cs
+//
+
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Touch")]
+public class TouchCameraControl : MonoBehaviour
+{
+	public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+	public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+
+	public float orthoZoomMin = 1.5f;
+	public float orthoZoomMax= 8.0f;
+
+	public float perspectiveZoomMin= 15.0f;
+	public float perspectiveZoomMax= 80.0f;
+
+	void Update()
+	{
+	//	if (camera.isOrthoGraphic)
+	//		camera.orthographicSize = Mathf.Clamp (camera.orthographicSize, orthoZoomMin, orthoZoomMax);
+	//	else
+	//		camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, perspectiveZoomMin, 80.0f);
+	
+		// If there are two touches on the device...
+		if (Input.touchCount == 2)
+		{
+			// Store both touches.
+			Touch touchZero = Input.GetTouch(0);
+			Touch touchOne = Input.GetTouch(1);
+			
+			// Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+			
+			// Find the magnitude of the vector (the distance) between the touches in each frame.
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+			
+			// Find the difference in the distances between each frame.
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+			
+			// If the camera is orthographic...
+			if (camera.isOrthoGraphic)
+			{
+				// ... change the orthographic size based on the change in distance between the touches.
+				camera.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+				
+				// Make sure the orthographic size never drops below zero.
+				camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, orthoZoomMin, orthoZoomMax);
+			}
+			else
+			{
+				// Otherwise change the field of view based on the change in distance between the touches.
+				camera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+				
+				// Clamp the field of view to make sure it's between 0 and 180.
+				camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, perspectiveZoomMin, perspectiveZoomMax);
+			}
+		}
+	}
+}
