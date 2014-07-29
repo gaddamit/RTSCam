e925b157a1ba510658935bafb7031ff1cd57ea75
Leo Esguerra <leo.esguerra@gmail.com>
2014-07-29 23:13:41 +0800

    + Added combined script for mouse and keyboard controls

---
+++ Assets/scripts/RTSCameraControl.cs
@@ -0,0 +1,242 @@
+﻿//
+//Filename: MouseCameraControl.cs
+//
+
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/RTS")]
+public class RTSCameraControl : MonoBehaviour
+{ 
+	public SpriteRenderer mapSprite;
+
+	public float orthoZoomSensitivity = 5.0f;
+	public float orthoZoomSpeed = 3.0f;
+	public float orthoZoomMin = 1.5f;
+	public float orthoZoomMax= 8.0f;
+	
+	public float perspectiveZoomSensitivity= 30.0f;
+	public float perspectiveZoomSpeed= 5.0f;
+	public float perspectiveZoomMin= 15.0f;
+	public float perspectiveZoomMax= 80.0f;
+	
+	private float zoom;
+	private float zoomSensitivity;
+	private float zoomSpeed;
+	private float zoomMin;
+	private float zoomMax;
+
+	private Rect bounds;
+	
+	private float vertExtent;
+	private float horzExtent;
+
+	[System.Serializable]
+	// Handles left modifiers keys (Alt, Ctrl, Shift)
+	public class Modifiers
+	{
+		public bool leftAlt;
+		public bool leftControl;
+		public bool leftShift;
+		
+		public bool checkModifiers()
+		{
+			return (!leftAlt ^ Input.GetKey(KeyCode.LeftAlt)) &&
+				(!leftControl ^ Input.GetKey(KeyCode.LeftControl)) &&
+					(!leftShift ^ Input.GetKey(KeyCode.LeftShift));
+		}
+	}
+
+	// MOUSE CONFIGURATION
+	// Mouse buttons in the same order as Unity
+	public enum MouseButton { Left = 0, Right = 1, Middle = 2, None = 3 }
+	
+	[System.Serializable]
+	// Handles common parameters for translations and rotations
+	public class MouseControlConfiguration
+	{
+		public bool activate;
+		public MouseButton mouseButton;
+		public Modifiers modifiers;
+		public float sensitivity;
+		
+		public bool isActivated()
+		{
+			return activate && Input.GetMouseButton((int)mouseButton) && modifiers.checkModifiers();
+		}
+	}
+	
+	[System.Serializable]
+	// Handles scroll parameters
+	public class MouseScrollConfiguration
+	{
+		public bool activate;
+		public Modifiers modifiers;
+		public float sensitivity;
+		
+		public bool isActivated()
+		{
+			return activate && modifiers.checkModifiers();
+		}
+	}
+
+	// Vertical translation default configuration
+	public MouseControlConfiguration mouseVerticalTranslation = new MouseControlConfiguration { activate = true, mouseButton = MouseButton.Left, sensitivity = 0.25F };
+	
+	// Horizontal translation default configuration
+	public MouseControlConfiguration mouseHorizontalTranslation = new MouseControlConfiguration { activate = true, mouseButton = MouseButton.Left, sensitivity = 0.25F };
+
+	// Scroll default configuration
+	public MouseScrollConfiguration mouseScroll = new MouseScrollConfiguration { activate = true, sensitivity = 2F };
+	
+	// Default unity names for mouse axes
+	public string mouseHorizontalAxisName = "Mouse X";
+	public string mouseVerticalAxisName = "Mouse Y";
+	public string scrollAxisName = "Mouse ScrollWheel";
+
+	// END OF MOUSE CONFIGURATION
+
+	// KEYBOARD CONFIGURATION
+	// Keyboard axes buttons in the same order as Unity
+	public enum KeyboardAxis { Horizontal = 0, Vertical = 1, None = 3 }
+
+	[System.Serializable]
+	public class KeyboardControlConfiguration
+	{
+		public bool activate;
+		public KeyboardAxis keyboardAxis;
+		public Modifiers modifiers;
+		public float sensitivity;
+		
+		public bool isActivated()
+		{
+			return activate && keyboardAxis != KeyboardAxis.None && modifiers.checkModifiers();
+		}
+	}
+
+	// Vertical translation default configuration
+	public KeyboardControlConfiguration keyboardVerticalTranslation = new KeyboardControlConfiguration { activate = true, keyboardAxis = KeyboardAxis.Vertical, sensitivity = 0.5F };
+	
+	// Horizontal translation default configuration
+	public KeyboardControlConfiguration keyboardHorizontalTranslation = new KeyboardControlConfiguration { activate = true, keyboardAxis = KeyboardAxis.Horizontal, sensitivity = 0.5F };
+
+	// Default unity names for keyboard axes
+	public string keyboardHorizontalAxisName = "Horizontal";
+	public string keyboardVerticalAxisName = "Vertical";
+	public string[] keyboardAxesNames;
+	// END KEYBOARD CONFIGURATION
+
+	void Start() 
+	{
+		keyboardAxesNames = new string[] { keyboardHorizontalAxisName, keyboardVerticalAxisName};
+
+		InitBounds ();
+		InitZoom ();
+	}
+
+	void InitBounds()
+	{
+		float mapSpriteWidth = mapSprite.sprite.rect.width;
+		float mapSpriteHeight = mapSprite.sprite.rect.height;
+		
+		Vector3 mapVector = new Vector3 (mapSpriteWidth, mapSpriteHeight, camera.nearClipPlane);
+
+		camera.orthographicSize = Screen.height / 2.0f / 100;
+		
+		vertExtent = camera.orthographicSize;
+		horzExtent = vertExtent * camera.aspect;
+		
+		// Calculations assume map is at origin
+		bounds.xMin = (horzExtent - camera.ScreenToWorldPoint(mapVector).x) / 2.0f;
+		bounds.xMax = (camera.ScreenToWorldPoint(mapVector).x  - horzExtent) / 2.0f;
+
+		bounds.yMin = (vertExtent - camera.ScreenToWorldPoint(mapVector).y) / 2.0f;
+		bounds.yMax = (camera.ScreenToWorldPoint(mapVector).y - vertExtent) / 2.0f;
+	}
+
+	void InitZoom()
+	{
+		if (camera.isOrthoGraphic)
+		{
+			zoom = camera.orthographicSize;
+			zoomSensitivity = orthoZoomSensitivity;
+			zoomSpeed = orthoZoomSpeed;
+			zoomMin = orthoZoomMin;
+			zoomMax = orthoZoomMax;
+		} else {
+			zoom = camera.fieldOfView;
+			zoomSensitivity = perspectiveZoomSensitivity;
+			zoomSpeed = perspectiveZoomSpeed;
+			zoomMin = perspectiveZoomMin;
+			zoomMax = perspectiveZoomMax;
+		}
+	}
+
+	void Update()
+	{
+		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+		zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+	}
+	
+	void LateUpdate ()
+	{
+		if (mouseVerticalTranslation.isActivated())
+		{
+			float translateY = Input.GetAxis(mouseVerticalAxisName) * mouseVerticalTranslation.sensitivity;
+			transform.Translate(0, -translateY, 0);
+		}
+		
+		if (mouseHorizontalTranslation.isActivated())
+		{
+			float translateX = Input.GetAxis(mouseHorizontalAxisName) * mouseHorizontalTranslation.sensitivity;
+			transform.Translate(-translateX, 0, 0);
+		}
+		
+		if (mouseScroll.isActivated ()) 
+		{
+			if (camera.isOrthoGraphic)
+				camera.orthographicSize = Mathf.Lerp (camera.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
+			else
+				camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoom, Time.deltaTime * zoomSpeed);
+
+			AdjustBounds();
+		}
+
+		if (keyboardVerticalTranslation.isActivated())
+		{
+			float translateY = Input.GetAxis(keyboardAxesNames[(int)keyboardVerticalTranslation.keyboardAxis]) * keyboardVerticalTranslation.sensitivity;
+			transform.Translate(0, translateY, 0);
+		}
+
+		if (keyboardHorizontalTranslation.isActivated())
+		{
+			float translateX = Input.GetAxis(keyboardAxesNames[(int)keyboardHorizontalTranslation.keyboardAxis]) * keyboardHorizontalTranslation.sensitivity;
+			transform.Translate(translateX, 0, 0);
+		}
+
+		Move();
+	}
+
+	void AdjustBounds()
+	{
+		vertExtent = camera.orthographicSize;
+		horzExtent = vertExtent * camera.aspect;
+		
+		bounds.yMin = vertExtent - zoomMax;
+		bounds.yMin = Mathf.Clamp (bounds.yMin, -100, 0);
+		bounds.yMax = zoomMax - vertExtent;
+		bounds.yMax = Mathf.Clamp (bounds.yMax, 0, 100);
+
+		bounds.xMin = bounds.yMin * camera.aspect;
+		bounds.xMin = Mathf.Clamp (bounds.xMin, -100, 0);
+		bounds.xMax = bounds.yMax * camera.aspect;
+		bounds.xMax = Mathf.Clamp (bounds.xMax, 0, 100);
+	}
+
+	void Move()
+	{
+		Vector3 v3 = transform.localPosition;
+		v3.x = Mathf.Clamp(v3.x, bounds.xMin, bounds.xMax);
+		v3.y = Mathf.Clamp(v3.y, bounds.yMin, bounds.yMax);
+		transform.localPosition = v3;
+	}
+}
